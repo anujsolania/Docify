341e13e9daf0e29c7af770b63e4c72e3af7d5114
anuj solania <anujsolania0507@gmail.com>
2025-06-22 15:52:49 +0530

    tiptapEditor extensions

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,11 +1,13 @@
+import { Editor } from "./editor";
+
 interface documentprops {
     params: Promise<{documentId: string}>;
 }
 const docidpage = async ({params}: documentprops) => {
     const { documentId } = await params;
     return ( 
-        <div>
-            Document Id: { documentId }
+        <div className="min-h-screen bg-[#FAFBFD]">
+            <Editor></Editor>
         </div>
      );
 }
