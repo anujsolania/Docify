bd0f109c66be92bb5f435d86bc6f0925e1157fb4
anuj solania <anujsolania0507@gmail.com>
2025-07-14 23:55:44 +0530

    searchdocs backend route

--- server/src/actions/document.actions.ts
+++ server/src/actions/document.actions.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { CustomRequest } from "../interfaces/interfacess";
+import { error } from "console";
 
 const prisma = new PrismaClient()
 
@@ -24,6 +25,37 @@ export const createdocument = async (req:CustomRequest, res:Response) => {
     }
 }
 
-export const getdocument = async (req:Request, res:Response) => {
+export const getdocuments = async (req:CustomRequest, res:Response) => {
+    try { 
+        const documents = await prisma.document.findMany({
+            where : {
+                userId: req.userId
+            }
+        })
+        return res.status(200).json({documents})
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({error: (error as Error).message})
+    }
+}
 
+export const searchdocuments = async (req: Request, res: Response) => {
+    const {filter} = req.query
+
+    if(!filter) return res.status(400).json({error: "search data not received"})
+    
+    try {
+        const documents = await prisma.document.findMany({
+            where: {
+                title: {
+                    contains: filter as string,
+                    mode: "insensitive"
+                }
+            }
+        })
+        return res.status(200).json({documents})
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({error: (error as Error).message})
+    }
 }
--- server/src/routes/document.router.ts
+++ server/src/routes/document.router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { documentSchema, docValidator } from "../validate/document.validate";
 import { Authorization } from "../middleware/authorization.middleware";
-import { createdocument, getdocument } from "../actions/document.actions";
+import { createdocument,getdocuments, searchdocuments } from "../actions/document.actions";
 const docRouter = express.Router()
 
-docRouter.post("/create",docValidator(documentSchema),Authorization,createdocument)
-docRouter.get("/",Authorization,getdocument)
+docRouter.post("/",docValidator(documentSchema),Authorization,createdocument)
+docRouter.get("/",Authorization,getdocuments)
+docRouter.get("/search",Authorization,searchdocuments)
 
 export default docRouter
