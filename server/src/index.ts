a0223b4111d7c2d83f0ac4c64623517df9993519
anuj solania <anujsolania0507@gmail.com>
2025-12-07 03:01:54 +0530

    OPTIONS preflight req fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,32 @@ dotenv.config()
 
 const app = express()
 
-app.use(express.json())
-app.use(cors({
-    origin: `${process.env.LINK}`,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-}))
+// app.use(express.json())
 
+// // app.use(cors())
+// app.use(cors({
+//     origin: `${process.env.LINK}`,
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+// }))
+
+// app.use("/api/v1", router)
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://docify-frontend.vercel.app"
+  ],
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+  credentials: true
+}));
+
+app.options("*", cors()); // preflight
+
+app.use(express.json());
+
+// Routes here
 app.use("/api/v1", router)
 
 // app.listen(3000, () => {
